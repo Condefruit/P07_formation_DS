37cb70e01a06fc15497fa5ddf349ac18ad11393a
DC <danielchastanet@gmail.com>
2022-03-30 00:38:16 +0200

    First commit28

--- setup.sh
+++ setup.sh
@@ -1,9 +1,3 @@
 mkdir -p ~/.streamlit/
 
-echo "\
-[server]\n\
-headless = true\n\
-port = 8501\n\
-enableCORS = false\n\
-\n\
-" > ~/.streamlit/config.toml
+echo "[server]\nheadless = true\nport = 8501\nenableCORS = false\n" > ~/.streamlit/config.toml
