8b9121e70d9d824ef5fe19d7883489272f232096
DC <danielchastanet@gmail.com>
2022-03-29 17:24:46 +0200

    First commit7

--- App.py
+++ App.py
@@ -1,7 +1,3 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
-from sklearn.tree import DecisionTreeClassifier
-import plotly.express as px
 
 st.title('Welcome to the credit answer dashboard !')
