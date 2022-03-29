f8fc5cae6cb1df0646fc67efc98520b6e9c09430
DC <danielchastanet@gmail.com>
2022-03-29 17:14:11 +0200

    First commit6

--- App.py
+++ App.py
@@ -1,10 +1,7 @@
-from flask import Flask, render_template
+import streamlit as st
+import pandas as pd
+import numpy as np
+from sklearn.tree import DecisionTreeClassifier
+import plotly.express as px
 
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-if __name__ == "__main__" :
-    app.run(debug=True)
+st.title('Welcome to the credit answer dashboard !')
