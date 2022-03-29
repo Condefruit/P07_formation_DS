9af93631340f6064e519dcf850b79cbbf0c94431
DC <danielchastanet@gmail.com>
2022-03-29 23:55:41 +0200

    First commit28

--- App.py
+++ App.py
@@ -2,4 +2,8 @@ import streamlit as st
 
 st.title('Welcome to the credit answer dashboard')
 
-run(host='0.0.0.0', port=os.environ.get('PORT', '5000'))
+port=os.environ.get('PORT', '8501')
+print(port)
+
+run(host='0.0.0.0', port=port)
+
