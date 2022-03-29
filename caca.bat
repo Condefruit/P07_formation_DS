4d9c597c213ec3491073747cea7514767b7035e4
DC <danielchastanet@gmail.com>
2022-03-30 00:48:32 +0200

    First commit28

---
+++ caca.bat
@@ -0,0 +1,4 @@
+git add .
+git commit -m "First commit28"
+git push heroku main
+heroku logs --tail
--- setup.sh
+++ setup.sh
@@ -1,3 +1,7 @@
 mkdir -p ~/.streamlit/
 
-echo "[server]\nheadless = true\nport = $PORT\nenableCORS = false\n" > ~/.streamlit/config.toml
+echo "[server]\n\
+headless = true\n\
+port = $PORT\n\
+enableCORS = false\n\
+" > ~/.streamlit/config.toml
