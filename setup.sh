7ead6d472a30997a4ee581ca0c75a41b05e2271b
DC <danielchastanet@gmail.com>
2022-03-29 15:49:20 +0200

    First commit

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 mkdir -p ~/.streamlit/
-echo “\
+echo "\
 [general]\n\
-email = \”your-email@domain.com\”\n\
-“ > ~/.streamlit/credentials.toml
-echo “\
+email = \"your-email@domain.com\"\n\
+" > ~/.streamlit/credentials.toml
+echo "\
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
