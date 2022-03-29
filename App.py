49f7cb631a8d9930cfea3e3471a63c1bb7c75f77
DC <danielchastanet@gmail.com>
2022-03-30 01:03:01 +0200

    First commit2587

--- App.py
+++ App.py
@@ -1,9 +1,3 @@
 import streamlit as st
 
-<<<<<<< HEAD
 st.title('Welcome to the credit answer dashboard')
-
-
-=======
-st.title('Welcome to the credit answer dashboard !')
->>>>>>> 8ea029cff976a89910010baba98d276f85c199a3
