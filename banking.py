1a54812b91c56bb70f960295b28ef151a5fcb596
DC <danielchastanet@gmail.com>
2022-03-30 18:51:25 +0200

    connecr

--- banking.py
+++ banking.py
@@ -5,6 +5,8 @@ import s3fs
 # `anon=False` means not anonymous, i.e. it uses access keys to pull data.
 fs = s3fs.S3FileSystem(anon=False)
 
+st.write("AWS ID:", st.secrets["AWS_ACCESS_KEY_ID"])
+
 # Retrieve file contents.
 # Uses st.experimental_memo to only rerun when the query changes or after 10 min.
 @st.experimental_memo(ttl=600)
