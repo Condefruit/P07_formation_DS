c2906ad4401138da7f097e03894a9087eb791b87
DC <danielchastanet@gmail.com>
2022-03-30 17:33:59 +0200

    com

--- App.py
+++ App.py
@@ -1,3 +1,20 @@
 import streamlit as st
+import s3fs
+
+# Create connection object.
+# `anon=False` means not anonymous, i.e. it uses access keys to pull data.
+fs = s3fs.S3FileSystem(anon=False)
+
+# Retrieve file contents.
+# Uses st.experimental_memo to only rerun when the query changes or after 10 min.
+@st.experimental_memo(ttl=600)
+def read_file(filename):
+    with fs.open(filename) as f:
+        return f.read().decode("utf-8")
 
 st.title('Welcome to the credit answer dashboard')
+
+df = read_file("p07oc/X_test.csv")
+
+# Print results.
+st.dataframe(df)
