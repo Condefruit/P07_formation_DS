5c71f33bac6cd3bbfcd036be0235c006b87c6226
DC <danielchastanet@gmail.com>
2022-03-31 08:34:46 +0200

    connecr

--- banking.py
+++ banking.py
@@ -2,23 +2,21 @@ import streamlit as st
 import pandas
 import s3fs
 import boto3
-import io
 
 # Create connection object.
 # `anon=False` means not anonymous, i.e. it uses access keys to pull data.
 fs = s3fs.S3FileSystem(anon=False)
 
-st.write("AWS ID:", st.secrets["AWS_ACCESS_KEY_ID"])
+# Retrieve file contents.
+# Uses st.experimental_memo to only rerun when the query changes or after 10 min.
+@st.experimental_memo(ttl=600)
+def read_file(filename):
+    with fs.open(filename) as f:
+        return f.read().decode("utf-8")
 
-s3_file_key = 'X_test.csv'
-bucket = 'p07oc'
+df = read_file("p07oc/X_test.csv.csv")
 
-s3 = boto3.client('s3')
-obj = s3.get_object(Bucket=bucket, Key=s3_file_key)
-
-df = pd.read_csv(io.BytesIO(obj['Body'].read()))
-
-# Print results.
 st.dataframe(df)
 
+# https://p07oc.s3.eu-west-3.amazonaws.com/X_test.csv
 
