ebcd2ecf8f0dafcf2934204e5eee62c8f1c60247
DC <danielchastanet@gmail.com>
2022-04-06 01:35:26 +0200

    up

--- banking.py
+++ banking.py
@@ -73,10 +73,6 @@ st.sidebar.write(amount)
 # ----------------------------------------
 # ----------------------------------------
 
-test = X_train.copy()
-test = test.set_index('Unnamed: 0')
-st.dataframe(test.head(3))
-
 
 st.title('Welcome to the credit answer dashboard !')
 
