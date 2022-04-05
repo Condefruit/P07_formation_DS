85ddfd42d2ae159402882f180af0dab352219783
DC <danielchastanet@gmail.com>
2022-04-06 00:47:49 +0200

    update

--- App.py
+++ App.py
@@ -22,22 +22,24 @@ st.write("ok tout continu de marcher")
 # Instruction de routage '/predict' = chemin predict ++> "POST" pour recevoir des données utilisateur
 @app.route('/predict', methods=["POST"])
 def prediction():
-    data = request.get_json()
-    prediction_value = np.array2string(model.predict_proba(data)[0, 1])
+    #data = request.get_json()
+    #prediction_value = np.array2string(model.predict_proba(data)[0, 1])
 
-    return jsonify(prediction_value)
+    #return jsonify(prediction_value)
+    return 1
 
 # Instruction de routage
 @app.route('/explain', methods=["POST"])
 def explain():
-    data_client = request.json
-    data_client_values = np.array([list(data_client.values())])
-    data_client_features = list(data_client.keys())
-    explainer_shap = shap.TreeExplainer(model)
-    shap_values_client = explainer_shap.shap_values(data_client_values)
-    shap_values_client_serie = pd.Series(index=data_client_features, data=shap_values_client[1][0, :])
+    #data_client = request.json
+    #data_client_values = np.array([list(data_client.values())])
+    #data_client_features = list(data_client.keys())
+    #explainer_shap = shap.TreeExplainer(model)
+    #shap_values_client = explainer_shap.shap_values(data_client_values)
+    #shap_values_client_serie = pd.Series(index=data_client_features, data=shap_values_client[1][0, :])
 
-    return jsonify(shap_values_client_serie.to_dict())
+    #return jsonify(shap_values_client_serie.to_dict())
+    return 2
     
 st.write('yes')
     
