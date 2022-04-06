53be0161457a3fab31999f2a9e7ce178f671d8a0
DC <danielchastanet@gmail.com>
2022-04-06 20:57:36 +0200

    up

--- App.py
+++ App.py
@@ -19,7 +19,9 @@ model = pickle.load(pickle_in)
 @app.route('/predict', methods=["POST"])
 def prediction():
     data = request.get_json() # Parses the incoming JSON request data and returns it. / convertit l’objet JSON en données Python
+    print(data)
     prediction_value = np.array2string(model.predict_proba(data)[0, 1]) # retourne la proba de la class 1
+    print(prediction_value)
 
     return jsonify(prediction_value) # Serialize data to JSON
 
