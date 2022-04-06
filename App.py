76f9732c070a8e642c137a7b38d8d4a6d3b2d2df
DC <danielchastanet@gmail.com>
2022-04-06 15:18:03 +0200

    update

--- App.py
+++ App.py
@@ -1,3 +1,8 @@
+import numpy as np
+import pandas as pd
+import shap
+import pickle5 as pickle
+from lightgbm import LGBMClassifier
 from flask import Flask, request, jsonify
 
 # Crée l’objet application Flask, qui contient les données de l’application et les méthodes.
@@ -5,6 +10,10 @@ app = Flask(__name__)
 # Modyfying part of the original config / check print(app.config)
 app.config["DEBUG"] = True
 #app.config['JSON_SORT_KEYS'] = False
+
+# Load the Pickle file in the memory
+pickle_in = open('best_model.pickle', 'rb')
+model = pickle.load(pickle_in)
 
 @app.route("/")
 def hello_world():
