f0b396ac9e8f3a4f170378e05815d948b31109f6
DC <danielchastanet@gmail.com>
2022-04-06 15:12:13 +0200

    update

--- App.py
+++ App.py
@@ -1,9 +1,4 @@
-import numpy as np
-import pandas as pd
-import shap
-import pickle5 as pickle
 from flask import Flask, request, jsonify
-from lightgbm import LGBMClassifier
 
 # Crée l’objet application Flask, qui contient les données de l’application et les méthodes.
 app = Flask(__name__)
@@ -11,7 +6,7 @@ app = Flask(__name__)
 app.config["DEBUG"] = True
 #app.config['JSON_SORT_KEYS'] = False
 
-@app.route("")
+@app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
 
