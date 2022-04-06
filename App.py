d0a5f653c0152c3112f89e0a4cf7677973480cc9
DC <danielchastanet@gmail.com>
2022-04-06 23:37:24 +0200

    up

--- App.py
+++ App.py
@@ -19,9 +19,9 @@ model = pickle.load(pickle_in)
 @app.route('/predict', methods=["POST"])
 def prediction():
     data = request.get_json() # Parses the incoming JSON request data and returns it. / convertit l’objet JSON en données Python
-    print(data)
+    #print(data)
     prediction_value = np.array2string(model.predict_proba(data)[0, 1]) # retourne la proba de la class 1
-    print(prediction_value)
+    #print(prediction_value)
 
     return jsonify(prediction_value) # Serialize data to JSON
 
@@ -29,11 +29,17 @@ def prediction():
 @app.route('/explain', methods=["POST"])
 def explain():
     data_client = request.json
+    print("1", data_client)
     data_client_values = np.array([list(data_client.values())])
+    print("2", ata_client_values)
     data_client_features = list(data_client.keys())
+    print("3", data_client_features)
     explainer_shap = shap.TreeExplainer(model)
+    print("4", explainer_shap) 
     shap_values_client = explainer_shap.shap_values(data_client_values)
+    print("5", shap_values_client)
     shap_values_client_serie = pd.Series(index=data_client_features, data=shap_values_client[1][0, :])
+    print("6", shap_values_client_serie)
 
     return jsonify(shap_values_client_serie.to_dict())
 
