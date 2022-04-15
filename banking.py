101febf7cbd54d7c4026870ea4fb6b61abe62d31
DC <danielchastanet@gmail.com>
2022-04-15 02:43:46 +0200

    up

--- banking.py
+++ banking.py
@@ -77,21 +77,25 @@ nb_features_explain = st.sidebar.slider('Number of explanation features', min_va
 
 st.sidebar.markdown("""---""")
 
-amount = X_test['AMT_CREDIT'].loc[customer_number]
-ab = 6
+# Partie changement montant crédit
+# ----------------------------------------
+# ----------------------------------------
 
-cola, colb = st.sidebar.columns(2)
+# amount = X_test['AMT_CREDIT'].loc[customer_number]
+# ab = 6
 
-with cola:
-    st.header(amount)
-    st.write(ab)
+# cola, colb = st.sidebar.columns(2)
 
-with colb:
-    new_amount = st.number_input('New credit amount', min_value = 1)
+# with cola:
+#     st.header(amount)
+#     st.write(ab)
 
-update = st.sidebar.button('Change Amount')
-if update:
-    ab = new_amount
+# with colb:
+#     new_amount = st.number_input('New credit amount', min_value = 1)
+
+# update = st.sidebar.button('Change Amount')
+# if update:
+#     ab = new_amount
 
 
 # Communication with the API
@@ -204,31 +208,3 @@ fig3 = px.scatter(X_test, x = select_element1, y = select_element2)
 fig3.add_vline(x = X_test[select_element1].loc[customer_number], line_width = 1, line_color = 'red')
 fig3.add_hline(y = X_test[select_element2].loc[customer_number], line_width = 1, line_color = 'red')
 st.write(fig3)
-
-import streamlit.components.v1 as components
-import xgboost
-
-@st.cache
-def load_data():
-    return shap.datasets.boston()
-
-def st_shap(plot, height=None):
-    shap_html = f"<head>{shap.getjs()}</head><body>{plot.html()}</body>"
-    components.html(shap_html, height=height)
-
-st.title("SHAP in Streamlit")
-
-# train XGBoost model
-X,y = load_data()
-model = xgboost.train({"learning_rate": 0.01}, xgboost.DMatrix(X, label=y), 100)
-
-# explain the model's predictions using SHAP
-# (same syntax works for LightGBM, CatBoost, scikit-learn and spark models)
-explainer = shap.TreeExplainer(model)
-shap_values = explainer.shap_values(X)
-
-# visualize the first prediction's explanation (use matplotlib=True to avoid Javascript)
-st_shap(shap.force_plot(explainer.expected_value, shap_values[0,:], X.iloc[0,:]))
-
-# visualize the training set predictions
-st_shap(shap.force_plot(explainer.expected_value, shap_values, X), 400)
